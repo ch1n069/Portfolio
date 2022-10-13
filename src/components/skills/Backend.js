fef71bc2aa95d097b1e9086f3aae2234ee61465c
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-13 20:06:50 +0300

    created the backend and front end components

---
+++ src/components/skills/Backend.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title"></h3>
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+          </div>
+          <div>
+            <h3 className="skills__name">Django</h3>
+            <span className="skills__level">advanced</span>
+          </div>
+        </div>
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+          </div>
+          <div>
+            <h3 className="skills__name">Python</h3>
+            <span className="skills__level">Advanced</span>
+          </div>
+        </div>
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+          </div>
+          <div>
+            <h3 className="skills__name">PostgresQl</h3>
+            <span className="skills__level">Advanced</span>
+          </div>
+        </div>
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+          </div>
+          <div>
+            <h3 className="skills__name">Mysql</h3>
+            <span className="skills__level">Advanced</span>
+          </div>
+        </div>
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+          </div>
+          <div>
+            <h3 className="skills__name">NodeJs</h3>
+            <span className="skills__level">Intermediate</span>
+          </div>
+        </div>
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+          </div>
+          <div>
+            <h3 className="skills__name">PHP</h3>
+            <span className="skills__level">Intermediate</span>
+          </div>
+        </div>
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+          </div>
+          <div>
+            <h3 className="skills__name">Firebase</h3>
+            <span className="skills__level">Advanced</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;

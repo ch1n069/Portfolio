b0243517ada73f70b819dc964a2fb540c2a5d4c1
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-13 12:25:11 +0300

    created the header section and the li items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Hero from "./Components/Hero";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div>
-      <Hero />
+      <Header />
     </div>
   );
 }

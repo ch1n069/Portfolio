94e1c11268965b4c722fc77df088bc679442c00b
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-13 13:54:24 +0300

    partioned the home section

---
+++ src/components/home/Data.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">Bruno Gatiba</h1>
+      <h3 className="home__subtitle">Fullstack webdeveloper</h3>
+      <p className="home__description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nihil
+        iure officia id sint aliquid rem magni ipsa quam provident.
+      </p>
+      <a href="#contact" className="button button--flex">
+        Say hello
+      </a>
+    </div>
+  );
+};
+
+export default data;
---
+++ src/components/home/Social.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="34" className="home__social-icon" target="_blank">
+        <i class="uil uil-instagram" style="cursor:pointer"></i>
+      </a>
+
+      <a href="34" className="home__social-icon" target="_blank">
+        <i class="uil uil-dribble" style="cursor:pointer"></i>
+      </a>
+      <a href="34" className="home__social-icon" target="_blank">
+        <i class="uil uil-github-alt" style="cursor:pointer"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;

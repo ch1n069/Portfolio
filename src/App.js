a2e6093df133ea15e75605008200c351bde3b479
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-13 12:53:16 +0300

    imported the header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Hero from "./Components/Hero";
+// import Hero from "./Components/Hero";
 import Header from "./components/header/Header";
 
 function App() {

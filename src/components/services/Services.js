b454dee84ca46defdfbc86cc372a3f000c29388c
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-15 18:54:11 +0300

    added responsiveness for small and medium devices

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -78,6 +78,135 @@ const Services = () => {
             </div>
           </div>
         </div>
+        <div className="services__content">
+          <div>
+            <i className="uil uil-web-grid services__icon"></i>
+            <h3 className="services__title">
+              Product <br />
+              Design
+            </h3>
+          </div>
+          <span
+            className="services__button"
+            onClick={() => {
+              modalHandler(1);
+            }}
+          >
+            {" "}
+            View more
+            <i className="uil uil-arrow-right"></i>
+          </span>
+
+          <div
+            className={
+              modal === 1 ? "services__modal active-modal" : "services__modal"
+            }
+          >
+            <div className="services__modal-content">
+              <i
+                onClick={() => {
+                  modalHandler(0);
+                }}
+                className="uil uil-times services__modal-close"
+              ></i>
+
+              <h3 className="services__modal-title">
+                {" "}
+                Product <br />
+                Design
+              </h3>
+              <p className="services__modal-description">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati, explicabo!
+              </p>
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="services__content">
+          <div>
+            <i className="uil uil-web-grid services__icon"></i>
+            <h3 className="services__title">
+              Product <br />
+              Design
+            </h3>
+          </div>
+          <span
+            className="services__button"
+            onClick={() => {
+              modalHandler(1);
+            }}
+          >
+            {" "}
+            View more
+            <i className="uil uil-arrow-right"></i>
+          </span>
+
+          <div
+            className={
+              modal === 1 ? "services__modal active-modal" : "services__modal"
+            }
+          >
+            <div className="services__modal-content">
+              <i
+                onClick={() => {
+                  modalHandler(0);
+                }}
+                className="uil uil-times services__modal-close"
+              ></i>
+
+              <h3 className="services__modal-title">
+                {" "}
+                Product <br />
+                Design
+              </h3>
+              <p className="services__modal-description">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati, explicabo!
+              </p>
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
         {/* <div className="services__content">
           <div>
             <i className="uil uil-web-grid services__icon"></i>

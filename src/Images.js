f13f86b602832472e28670e49a2d43596925da8c
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-16 00:09:26 +0300

    added framer motion library

--- src/Images.js
+++ src/Images.js
@@ -1,1 +1,8 @@
-import image1 from "./assets/";
+import image1 from "./assets/image1";
+import image2 from "./assets/image-2";
+import image3 from "./assets/image-3";
+import image4 from "./assets/image-4";
+import image5 from "./assets/image-5";
+import image6 from "./assets/image-6";
+
+export default [image1, image2, image3, image4, image5, image6];
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { motion } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+// import { motion } from "framer-motion";
+import {motion}
+// import { useEffect, useRef, useState } from "react";
 // imports happen above
 const Projects = () => {
   return (
     <section className="projects container section">
       <h2 className="section__title">Projects</h2>
+      <span className="section__subtitle">
+        Drag to scroll through projects{" "}
+      </span>
     </section>
   );
 };

e08e2529e1ff7ac0d0e13f8ba4190179511e959a
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-15 21:50:23 +0300

    styled the line for the education section

--- src/components/qualification/Qualification.js
+++ src/components/qualification/Qualification.js
@@ -4,32 +4,28 @@ import "./Qualification.css";
 const Qualification = () => {
   return (
     <section className="qualification section">
-      <h2 className="section__title">Qualification</h2>
-      <span className="section__subtitle">My personal Journey</span>
+      <h2 className="section__title">Qualifications</h2>
+      <span className="section__subtitle">My personal journey</span>
+
       <div className="qualification__container container">
         <div className="qualification__tabs">
           <div className="qualification__button qualification__active button--flex">
-            <i className="uil uil-graduation-cap qualification__icon"></i>
-            {""}
-            Education
+            <i className="uil uil-graduation-cap"></i>
+            {""}Education
           </div>
-          <div className="qualification__button qualification__active button--flex">
-            <i className="uil uil-graduation-cap qualification__icon"></i>
-            {""}
-            Experience
+          <div className="qualification__button button--flex">
+            <i className="uil uil-graduation-cap"></i>
+            {""}Experience
           </div>
         </div>
         <div className="qualification__sections">
           <div className="qualification__content qualification__content-active">
             <div className="qualification__data">
-              <div></div>
               <div>
-                <h4 className="quailification__title">
-                  Fullstack web developer
-                </h4>
-                <span className="qualification__subtitle">Kenya</span>
+                <h3 className="qualification__title">Web developers</h3>
+                <span className="qualification__subtitle">moringa school</span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt">2021-present</i>
+                  <div className="uil uil-calendar-alt">2021 -present</div>
                 </div>
               </div>
               <div>
@@ -39,47 +35,58 @@ const Qualification = () => {
             </div>
             <div className="qualification__data">
               <div></div>
+
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
               </div>
+
               <div>
-                <h4 className="quailification__title">
-                  Fullstack web developer
-                </h4>
-                <span className="qualification__subtitle">Kenya</span>
+                <h3 className="qualification__title">Web developers</h3>
+                <span className="qualification__subtitle">moringa school</span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt">2021-present</i>
+                  <div className="uil uil-calendar-alt">2021 -present</div>
                 </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Web developers</h3>
+                <span className="qualification__subtitle">moringa school</span>
+                <div className="qualification__calendar">
+                  <div className="uil uil-calendar-alt">2021 -present</div>
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
               </div>
             </div>
             <div className="qualification__data">
               <div></div>
+
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
               </div>
+
               <div>
-                <h4 className="quailification__title">
-                  Fullstack web developer
-                </h4>
-                <span className="qualification__subtitle">Kenya</span>
+                <h3 className="qualification__title">Web developers</h3>
+                <span className="qualification__subtitle">moringa school</span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt">2021-present</i>
+                  <div className="uil uil-calendar-alt">2021 -present</div>
                 </div>
               </div>
             </div>
           </div>
           <div className="qualification__content">
             <div className="qualification__data">
-              <div></div>
               <div>
-                <h4 className="quailification__title">
-                  Fullstack web developer
-                </h4>
-                <span className="qualification__subtitle">Kenya</span>
+                <h3 className="qualification__title">Web developers</h3>
+                <span className="qualification__subtitle">moringa school</span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt">2021-present</i>
+                  <div className="uil uil-calendar-alt">2021 -present</div>
                 </div>
               </div>
               <div>
@@ -89,33 +96,47 @@ const Qualification = () => {
             </div>
             <div className="qualification__data">
               <div></div>
+
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
               </div>
+
               <div>
-                <h4 className="quailification__title">
-                  Fullstack web developer
-                </h4>
-                <span className="qualification__subtitle">Kenya</span>
+                <h3 className="qualification__title">Web developers</h3>
+                <span className="qualification__subtitle">moringa school</span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt">2021-present</i>
+                  <div className="uil uil-calendar-alt">2021 -present</div>
                 </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Web developers</h3>
+                <span className="qualification__subtitle">moringa school</span>
+                <div className="qualification__calendar">
+                  <div className="uil uil-calendar-alt">2021 -present</div>
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
               </div>
             </div>
             <div className="qualification__data">
               <div></div>
+
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
               </div>
+
               <div>
-                <h4 className="quailification__title">
-                  Fullstack web developer
-                </h4>
-                <span className="qualification__subtitle">Kenya</span>
+                <h3 className="qualification__title">Web developers</h3>
+                <span className="qualification__subtitle">moringa school</span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt">2021-present</i>
+                  <div className="uil uil-calendar-alt">2021 -present</div>
                 </div>
               </div>
             </div>

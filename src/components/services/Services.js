11a0ad240dede32da2094225ce49c5ae445a260b
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-15 18:13:04 +0300

    minor refactoring

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Services.css";
 const Services = () => {
+  const [modal, setModalToOpen] = useState(0);
+
+  const modalHandler = (index) => {
+    setModalToOpen(index);
+  };
+
+  //   declaration of functions
   return (
     <section>
       <h2 className="section__title">Services</h2>
@@ -15,47 +22,63 @@ const Services = () => {
               Design
             </h3>
           </div>
-          <span className="services__button">
+          <span
+            className="services__button"
+            onClick={() => {
+              modalHandler(1);
+            }}
+          >
+            {" "}
             View more
-            <i className="uil uil-arrow-right services__button-icon"></i>
-            <div className="services__modal">
-              <div className="services__modal-content">
-                <i className="uil uil-times services__modal-close"></i>
-
-                <h3 className="services__modal-title">
-                  {" "}
-                  Product <br />
-                  Design
-                </h3>
-                <p className="services__modal-description">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Obcaecati, explicabo!
-                </p>
-                <ul className="services__modal-services grid">
-                  <li className="services__modal-service">
-                    <i className="uil uil-check-circle services__modal-icon"></i>
-                    <p className="services__modal-info">
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    </p>
-                  </li>
-                  <li className="services__modal-service">
-                    <i className="uil uil-check-circle services__modal-icon"></i>
-                    <p className="services__modal-info">
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    </p>
-                  </li>
-                  <li className="services__modal-service">
-                    <i className="uil uil-check-circle services__modal-icon"></i>
-                    <p className="services__modal-info">
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    </p>
-                  </li>
-                </ul>
-              </div>
-            </div>
+            <i className="uil uil-arrow-right"></i>
           </span>
+
+          <div
+            className={
+              modal === 1 ? "services__modal active-modal" : "services__modal"
+            }
+          >
+            <div className="services__modal-content">
+              <i
+                onClick={() => {
+                  modalHandler(0);
+                }}
+                className="uil uil-times services__modal-close"
+              ></i>
+
+              <h3 className="services__modal-title">
+                {" "}
+                Product <br />
+                Design
+              </h3>
+              <p className="services__modal-description">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati, explicabo!
+              </p>
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className="services__content">
+        {/* <div className="services__content">
           <div>
             <i className="uil uil-web-grid services__icon"></i>
             <h3 className="services__title">
@@ -149,8 +172,7 @@ const Services = () => {
                 </ul>
               </div>
             </div>
-          </span>
-        </div>
+        </div> */}
       </div>
     </section>
   );

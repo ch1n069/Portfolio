25b828cc1a4acb16185cc6dfb3eced5a4d9ee433
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-18 19:39:01 +0300

    adding of breakpoints

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -7,9 +7,9 @@ const Contact = () => {
       <span className="section__subtitle">Contact me </span>
 
       <div className="contact__container container grid">
-        <div className="contact__content">
+        {/* <div className="contact__content">
           <h3 className="contact__title">Talk to me</h3>
-        </div>
+        </div> */}
         <div className="contact__info">
           {/* <div className="contact__card">
             <i className="bx contact__card-icon"></i>

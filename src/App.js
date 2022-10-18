4322f351983363c37b210f8ff796a7b646820469
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-18 19:55:31 +0300

    created footer link

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Services from "./components/services/Services";
 import Qualification from "./components/qualification/Qualification";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Qualification />
         <Projects />
         <Contact />
+        <Footer />
       </main>
     </div>
   );
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// imports happen above
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Bruno </h1>
+        <ul className="footer__list"></ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

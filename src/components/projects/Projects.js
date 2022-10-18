f6e7713209ad04ad86514534a8af261bdedf77d2
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-18 13:47:09 +0300

    added motion div

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -7,17 +7,32 @@ import "./Projects.css";
 // import { useEffect, useRef, useState } from "react";
 // imports happen above
 const Projects = () => {
+  const [width, setWidth] = useState(0);
+
+  const carousel = useRef();
+  // const [current, setCurrent] = useState(0);
+
+  // const length = images.length;
+
+  // creating of functions
   return (
     <section className="projects container section">
       <h2 className="section__title">Projects</h2>
       <span className="section__subtitle">
         Drag to scroll through projects{" "}
       </span>
-      <div>
-        {images.map((image) => {
-          return <img src={image} alt="" />;
-        })}
-      </div>
+
+      <motion.div ref={carousel} className="carousel ">
+        <motion.div drag="x" className="inner-carousel slider">
+          {images.map((image) => {
+            return (
+              <motion.div className="item">
+                <img src={image} alt={image} />;
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
     </section>
   );
 };

c0b838092fac88dbc81e19ecc5a566d81de93549
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-18 19:58:31 +0300

    added social icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,7 +6,33 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__container container">
         <h1 className="footer__title">Bruno </h1>
-        <ul className="footer__list"></ul>
+        <ul className="footer__list">
+          <li>
+            <a href="" className="footer__link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="" className="footer__link">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="" className="footer__link"></a>
+          </li>
+        </ul>
+        <div className="footer__social">
+          <a href="34" className="home__social-icon" target="_blank">
+            <i class="uil uil-instagram"></i>
+          </a>
+
+          <a href="34" className="home__social-icon" target="_blank">
+            <i class="uil uil-dribble"></i>
+          </a>
+          <a href="34" className="home__social-icon" target="_blank">
+            <i class="uil uil-github-alt"></i>
+          </a>
+        </div>
       </div>
     </footer>
   );

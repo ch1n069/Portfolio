2b13cf50a78f0f746516fde4e537fc454e2d7632
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-09 13:37:24 +0300

    :bug: deployment bugs

--- src/components/home/Social.js
+++ src/components/home/Social.js
@@ -3,7 +3,7 @@ import React from "react";
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="34" className="home__social-icon" target="_blank">
+      <a href="34" className="home__social-icon" target="/">
         <i class="uil uil-instagram"></i>
       </a>
 

f60b95e2f3c7091d8139e71c7163262600100df7
Bruno Alpachino <brunolalpachino@gmail.com>
2024-07-22 03:06:28 +0300

    hero section update

--- src/components/home/Data.js
+++ src/components/home/Data.js
@@ -6,11 +6,11 @@ const data = () => {
       <h1 className="home__title">Bruno Gatiba</h1>
       <h3 className="home__subtitle">Software Engineer</h3>
       <p className="home__description">
-        I'm full-stack web developer based in Nairobi kenya , With a rich
+        I'm a full-stack web developer based in Nairobi kenya , With a rich
         background in full-stack web development, I proudly boast 2 years of
         hands-on experience dedicated to the art of creating sophisticated web
         applications. My professional journey is defined by a relentless pursuit
-        of excellence, marked by an unwavering attention to detail. I am not
+        of excellence, marked by an unwavering attention to detail. I'm not
         merely a web developer; I'm a craftsman.
       </p>
       <a href="#contact" className="button button--flex">
